795639ef176c662d49ea757257aa5d5978129422
kishan-7077 <150173474+kishan-7077@users.noreply.github.com>
2024-10-08 20:14:29 +0530

    add logic

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,18 @@
-async function addRepo() {
-	console.log("add command called");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function addRepo(filePath) {
+	const repoPath = path.resolve(process.cwd(), ".apnaGit");
+	const stagingPath = path.join(repoPath, "staging");
+
+	try {
+		await fs.mkdir(stagingPath, { recursive: true });
+		const fileName = path.basename(filePath);
+		await fs.copyFile(filePath, path.join(stagingPath, fileName));
+		console.log(`File ${fileName} added to the staging area!`);
+	} catch (err) {
+		console.error("error in adding file", err);
+	}
 }
 
 module.exports = { addRepo };
